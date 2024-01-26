7d686b10911c9f6dc86ec8de6790bd668b181245
Philip Gadzios <pgadzios@leftturnsolutions>
2024-01-26 15:00:13 +0000

    Initial commit of Study Management API before testing serverless deployment for sandbox

---
+++ NIHR.StudyManagement/NIHR.StudyManagement.Api/Configuration/JwtTokenValidationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace NIHR.StudyManagement.Api.Configuration
+{
+    public class JwtTokenValidationConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a property that, when true, will override and bypass Jwt token validation
+        /// allowing for local development without dependency on authentication server.
+        /// </summary>
+        public bool OverrideJwtTokenValidation { get; set; }
+
+        public string Authority { get; set; } = "";
+
+        public List<ClaimsConfiguration> OverrideClaimsConfigurations { get; set; } = new List<ClaimsConfiguration>();
+    }
+}
--- NIHR.StudyManagement/NIHR.StudyManagement.Api/Startup.cs
+++ NIHR.StudyManagement/NIHR.StudyManagement.Api/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using NIHR.StudyManagement.Api.Configuration;
-using NIHR.StudyManagement.Api.Middleware.Extensions;
 using System.Security.Claims;
 
 namespace NIHR.StudyManagement.Api;
-public class AuthenticationConfig
-{
-    public bool overrideJwtValidation { get; set; }
-}
+
 public class Startup
 {
     public Startup(IConfiguration configuration)
@@ -43,118 +39,21 @@ public class Startup
                 ValidateAudience = false
             };
 
-            //if (studyManagementApiConfiguration != null
-            //    && studyManagementApiConfiguration.JwtTokenValidationConfiguration.OverrideJwtTokenValidation)
-            //{
-            //    options.Events.ConfigureForLocalDevelopment(studyManagementApiConfiguration);
-            //}
-
-            //options.Events.ConfigureForLocalDevelopment();
-
+            // If local settings have a configuration value to override jwt token validation, then add
+            // some custom handlers to intercept jwt validation events. Note, this bypasses true authentication
+            // and should only be used in a local development environment. Claims can be mocked from the same configuration setting
             if(studyManagementApiConfiguration.JwtTokenValidationConfiguration.OverrideJwtTokenValidation)
             {
-                options.Events = new JwtBearerEvents()
+                var events = ConfigureForLocalDevelopment(studyManagementApiConfiguration);
+
+                if(events != null)
                 {
-                    OnMessageReceived = context =>
-                    {
-                        var claims = new List<Claim>();
-
-                        foreach (var claimConfig in studyManagementApiConfiguration.JwtTokenValidationConfiguration.ClaimsConfigurations)
-                        {
-                            claims.Add(new Claim(claimConfig.Name, claimConfig.Description));
-                        }
-
-                        context.Principal = new ClaimsPrincipal(
-                            new ClaimsIdentity(claims, context.Scheme.Name));
-
-                        context.Success();
-
-                        return Task.CompletedTask;
-                    },
-
-                    OnChallenge = context =>
-                    {
-                        return Task.CompletedTask;
-                    },
-
-                    OnForbidden = context =>
-                    {
-                        return Task.CompletedTask;
-                    },
-
-                    OnTokenValidated = context =>
-                    {
-                        return Task.CompletedTask;
-                    },
-
-                    OnAuthenticationFailed = context =>
-                    {
-                        return Task.CompletedTask;
-                    }
-                };
+                    options.Events = events;
+                }
             }
-
-            if (1 == 2)
-            {
-                options.Events = new JwtBearerEvents()
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var claims = new[]
-                            {
-                        new Claim(ClaimTypes.NameIdentifier,"LocalHostUser",ClaimValueTypes.String,context.Options.ClaimsIssuer),
-                        new Claim(ClaimTypes.Name,"LocalHostUser",ClaimValueTypes.String,context.Options.ClaimsIssuer)
-                        };
-
-                        context.Principal = new ClaimsPrincipal(
-                            new ClaimsIdentity(claims, context.Scheme.Name));
-
-                        context.Token = "ABC";
-
-                        context.Request.Headers["Authorization"] = "Bearer xxx";
-
-                        context.Success();
-
-                        return Task.CompletedTask;
-                    },
-
-                    OnChallenge = context =>
-                    {
-                        //context.HandleResponse();
-                        return Task.CompletedTask;
-                    },
-
-                    OnForbidden = context =>
-                    {
-                        //context.Success();
-                        return Task.CompletedTask;
-                    },
-
-                    OnTokenValidated = context =>
-                    {
-                        //context.Success();
-                        return Task.CompletedTask;
-                    },
-
-                    OnAuthenticationFailed = context =>
-                    {
-                        //context.Success();
-                        System.Diagnostics.Debug.Print($"OnAuthenticationFailed Exception: {context.Exception?.Message}");
-                        System.Diagnostics.Debug.Print($"OnAuthenticationFailed Failure: {context.Result?.Failure?.Message}");
-
-                        return Task.CompletedTask;
-                    }
-                };
-            }
-
         });
 
-        services.AddAuthorization(options =>
-        {
-            //options.AddPolicy("Admin", policy => policy.RequireClaim("cognito:groups", "AdminGroup"));
-
-            //options.AddPolicy(PolicyNames.StudyManagement_Api_Study_Create, policy => policy.Requirements.Add(new HasScopeRequirement(ScopeNames.StudyManagement_Api_Study_Create)));
-        });
+        services.AddAuthorization();
 
         services.AddEndpointsApiExplorer();
 
@@ -180,64 +79,62 @@ public class Startup
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
-            //endpoints.MapGet("/", async context =>
-            //{
-            //    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
-            //});
         });
     }
 
-    private static JwtBearerEvents ConfigureForLocalDevelopment(JwtBearerEvents jwtBearerEvents)
+    /// <summary>
+    /// This method is to override jwt token validation so that during local development there are no external dependencies, even
+    /// for authentication.
+    /// Configuration is controlled via appsettings values.
+    /// </summary>
+    /// <param name="studyManagementApiConfiguration"></param>
+    /// <returns></returns>
+    private static JwtBearerEvents? ConfigureForLocalDevelopment(StudyManagementApiConfiguration studyManagementApiConfiguration)
     {
-        jwtBearerEvents.OnMessageReceived = context =>
+        if(studyManagementApiConfiguration == null
+            || !studyManagementApiConfiguration.JwtTokenValidationConfiguration.OverrideJwtTokenValidation)
         {
-            var claims = new[]
+            return null;
+        }
+
+        return new JwtBearerEvents()
+        {
+            OnMessageReceived = context =>
+            {
+                var claims = new List<Claim>();
+
+                foreach (var claimConfig in studyManagementApiConfiguration.JwtTokenValidationConfiguration.OverrideClaimsConfigurations)
                 {
-                  new Claim(
-                      ClaimTypes.NameIdentifier,
-                      "LocalHostUser",
-                      ClaimValueTypes.String,
-                      context.Options.ClaimsIssuer),
-                  new Claim(
-                      ClaimTypes.Name,
-                      "LocalHostUser",
-                      ClaimValueTypes.String,
-                      context.Options.ClaimsIssuer)
-                };
-
-            context.Principal = new ClaimsPrincipal(
-                new ClaimsIdentity(claims, context.Scheme.Name));
-
-            context.Success();
-
-            return Task.CompletedTask;
-        };
-
-        return jwtBearerEvents;
-    }
-
-    private static void dosomething()
-    {
-        /*
-         *                 var claims = new[]
-                {
-                  new Claim(
-                      ClaimTypes.NameIdentifier,
-                      "LocalHostUser",
-                      ClaimValueTypes.String,
-                      context.Options.ClaimsIssuer),
-                  new Claim(
-                      ClaimTypes.Name,
-                      "LocalHostUser",
-                      ClaimValueTypes.String,
-                      context.Options.ClaimsIssuer)
-                };
+                    claims.Add(new Claim(claimConfig.Name, claimConfig.Description));
+                }
 
                 context.Principal = new ClaimsPrincipal(
                     new ClaimsIdentity(claims, context.Scheme.Name));
+
                 context.Success();
-              }
-              return Task.CompletedTask;
-         * */
+
+                return Task.CompletedTask;
+            },
+
+            OnChallenge = context =>
+            {
+                return Task.CompletedTask;
+            },
+
+            OnForbidden = context =>
+            {
+                return Task.CompletedTask;
+            },
+
+            OnTokenValidated = context =>
+            {
+                return Task.CompletedTask;
+            },
+
+            OnAuthenticationFailed = context =>
+            {
+                return Task.CompletedTask;
+            }
+        };
     }
 }
