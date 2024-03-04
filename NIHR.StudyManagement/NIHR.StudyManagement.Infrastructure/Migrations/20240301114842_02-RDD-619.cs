b6abe1176c28b4ae427a632af748109608014164
P Gadzios <pgadzios@leftturnsolutions.com>
2024-03-04 11:20:03 +0000

    Removed data updates automagically added by EF.

--- NIHR.StudyManagement/NIHR.StudyManagement.Infrastructure/Migrations/20240301114842_02-RDD-619.cs
+++ NIHR.StudyManagement/NIHR.StudyManagement.Infrastructure/Migrations/20240301114842_02-RDD-619.cs
@@ -33,55 +33,6 @@ namespace NIHR.StudyManagement.Infrastructure.Migrations
                 })
                 .Annotation("MySQL:Charset", "utf8mb4");
 
-            migrationBuilder.UpdateData(
-                table: "personRole",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 13, DateTimeKind.Local).AddTicks(6382));
-
-            migrationBuilder.UpdateData(
-                table: "personType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 13, DateTimeKind.Local).AddTicks(7023));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeIdentifierType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 14, DateTimeKind.Local).AddTicks(3080));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeIdentifierType",
-                keyColumn: "id",
-                keyValue: 2,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 14, DateTimeKind.Local).AddTicks(3094));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 14, DateTimeKind.Local).AddTicks(3469));
-
-            migrationBuilder.UpdateData(
-                table: "sourceSystem",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 14, DateTimeKind.Local).AddTicks(9837));
-
-            migrationBuilder.UpdateData(
-                table: "sourceSystem",
-                keyColumn: "id",
-                keyValue: 2,
-                column: "created",
-                value: new DateTime(2024, 3, 1, 11, 48, 42, 14, DateTimeKind.Local).AddTicks(9850));
-
             migrationBuilder.CreateIndex(
                 name: "IX_griResearchStudyStatus_GriMappingId",
                 table: "griResearchStudyStatus",
@@ -92,55 +43,6 @@ namespace NIHR.StudyManagement.Infrastructure.Migrations
         {
             migrationBuilder.DropTable(
                 name: "griResearchStudyStatus");
-
-            migrationBuilder.UpdateData(
-                table: "personRole",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 980, DateTimeKind.Local).AddTicks(5038));
-
-            migrationBuilder.UpdateData(
-                table: "personType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 980, DateTimeKind.Local).AddTicks(5695));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeIdentifierType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 981, DateTimeKind.Local).AddTicks(5896));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeIdentifierType",
-                keyColumn: "id",
-                keyValue: 2,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 981, DateTimeKind.Local).AddTicks(5922));
-
-            migrationBuilder.UpdateData(
-                table: "researchInitiativeType",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 981, DateTimeKind.Local).AddTicks(6579));
-
-            migrationBuilder.UpdateData(
-                table: "sourceSystem",
-                keyColumn: "id",
-                keyValue: 1,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 982, DateTimeKind.Local).AddTicks(8192));
-
-            migrationBuilder.UpdateData(
-                table: "sourceSystem",
-                keyColumn: "id",
-                keyValue: 2,
-                column: "created",
-                value: new DateTime(2024, 2, 20, 10, 33, 23, 982, DateTimeKind.Local).AddTicks(8221));
         }
     }
 }
