eee32b985b72e1fd521d1e008616f2a38bb9f15f
abhi-nihr <abhisheik.yarrakrishnappa@nihr.ac.uk>
2024-03-07 16:27:29 +0000

    Update DB script

--- DatabaseScripts/1-CreateInitialDatabase-RDDI-100.sql
+++ DatabaseScripts/1-CreateInitialDatabase-RDDI-100.sql
@@ -187,3 +187,28 @@ VALUES ('20240220103324_01-Initial', '6.0.25');
 
 COMMIT;
 
+START TRANSACTION;
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20240301114842_02-RDD-619')
+BEGIN
+    CREATE TABLE `griResearchStudyStatus` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `GriMappingId` int NOT NULL,
+        `code` varchar(100) NOT NULL,
+        `FromDate` datetime(6) NOT NULL,
+        `ToDate` datetime(6) NULL,
+        `created` datetime(6) NOT NULL,
+        PRIMARY KEY (`id`),
+        CONSTRAINT `fk_griResearchStudyStatus_griMapping` FOREIGN KEY (`GriMappingId`) REFERENCES `griMapping` (`id`)
+    );
+END;
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20240301114842_02-RDD-619')
+BEGIN
+    CREATE INDEX `IX_griResearchStudyStatus_GriMappingId` ON `griResearchStudyStatus` (`GriMappingId`);
+END;
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20240301114842_02-RDD-619')
+BEGIN
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20240301114842_02-RDD-619', '6.0.25');
+END;
+COMMIT;
+
